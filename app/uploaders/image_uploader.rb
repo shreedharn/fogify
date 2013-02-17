7d9a3322abd1d8e390f718443f74e622c68204ff
Shreedhar <Shreedharn@gmail.com>
2013-02-16 22:14:26 -0500

    Linked thumbnails to image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,7 +44,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [200, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
