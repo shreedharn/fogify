6719aced983fc48d87e7e7996706e12869abdce2
Shreedhar <Shreedharn@gmail.com>
2013-02-26 23:16:28 -0500

    facebook integration callback verified

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130226020745) do
+ActiveRecord::Schema.define(:version => 20130227035127) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "uname"
+    t.string   "uemail"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
