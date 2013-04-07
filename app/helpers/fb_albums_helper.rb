c167d9f4a8cf02aade50b884c35c8cb8a61a980f
Shreedhar Natarajan <shreedharn@gmail.com>
2013-04-07 19:35:55 -0400

    display favorite album added

--- app/helpers/fb_albums_helper.rb
+++ app/helpers/fb_albums_helper.rb
@@ -1,4 +1,17 @@
 module FbAlbumsHelper
 
+  #erb helper
+  def getimg_w_h (photos)
+    photo_wide,photo_tall = [], []
+    return nil if photos.nil?
+    photos.each do |photo|
+      if (photo['src_big_width'] > photo['src_big_height'])
+         photo_wide << photo
+      else
+         photo_tall << photo
+      end
+    end
+    return photo_wide,photo_tall
+  end
 
 end
