fc79c7e9e1385011c04537ba2efe5f6d9c8512be
Shreedhar Natarajan <shreedharn@gmail.com>
2013-04-07 19:35:55 -0400

    display favorite album added

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,9 @@
 class AlbumsController < ApplicationController
   # GET /albums
   # GET /albums.json
+  include FbAlbumsHelper #erb helper defined here
+  include FogifyHelper::GraphHelper  #graph helper defined here
+
   before_filter :authenticate_user!
 
   def index
@@ -8,11 +11,13 @@ class AlbumsController < ApplicationController
     this_auth =  user_auth.where(:uemail => current_user.email).first
 
     @graph = Koala::Facebook::API.new(this_auth.access_token)
-    @albums = @graph.get_object("me/albums")
+    #@albums = @graph.get_object("me/albums")
+    album_info = get_album_with_max_likes(@graph)
+    @photos =   get_pics_info(@graph,album_info['object_id'], album_info['photo_count'])
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: albums }
+      format.json { render json: @photos }
     end
 
   end
