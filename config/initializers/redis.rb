d19486f325f347ceb1cca62bee1f4a50bbe2f7ab
Shreedhar Natarajan <shreedharn@gmail.com>
2013-04-16 21:56:33 -0400

    Redis object init with uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,7 @@
-$redis = Redis.new
+redisUrl = ENV['REDISTOGO_URL']
+unless redisUrl.nil?
+  uri = URI.parse(redisUrl)
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  $redis = Redis.new
+end
